5e700bedf3b2782a5b51a4a83df367c36e12e003
hstoykova <hristina.pencheva@gmail.com>
2024-01-11 17:58:26 +0200

    "1. Reverse a String" solved

---
+++ Advanced/A-Lab-StacksAndQueues/1.ReverseAString/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+I Love C#
+ */
+
+namespace _1.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+} 
