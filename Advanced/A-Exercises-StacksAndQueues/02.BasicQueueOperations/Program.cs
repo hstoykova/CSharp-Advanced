33fa7f7b1726ffb913b73866057ad2334b581b54
hstoykova <hristina.pencheva@gmail.com>
2024-01-12 10:41:45 +0200

    "02. Basic Queue Operations" solved

---
+++ Advanced/A-Exercises-StacksAndQueues/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace _02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            // Add elements to the Queue:
+            Queue<int> integerQueue = new Queue<int>(secondLine);
+
+            // Remove elements from the Queue
+            for (int i = 0; i < firstLine[1]; i++)
+            {
+                integerQueue.Dequeue();
+            }
+            
+            // Do the checks and the output:
+            if (integerQueue.Contains(firstLine[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if (integerQueue.Any())
+            {
+                Console.WriteLine(integerQueue.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
