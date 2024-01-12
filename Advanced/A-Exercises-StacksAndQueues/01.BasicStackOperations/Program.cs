6803450601cf7780a08c312efa2908f1209c5b07
hstoykova <hristina.pencheva@gmail.com>
2024-01-12 10:26:16 +0200

    "01. Basic Stack Operations" solved

---
+++ Advanced/A-Exercises-StacksAndQueues/01.BasicStackOperations/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            string[] secondLine = Console.ReadLine().Split();
+
+            Stack<int> integerStack = new Stack<int>();
+
+            // Add all elements to the stack
+            foreach (var s in secondLine)
+            {
+                int temp = int.Parse(s);
+                integerStack.Push(temp);
+            }
+
+            // Remove s from stack
+            for (int i = 0; i < input[1]; i++)
+            {
+                integerStack.Pop();
+            }
+
+            if (integerStack.Contains(input[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if (integerStack.Any())
+            {
+                Console.WriteLine(integerStack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
