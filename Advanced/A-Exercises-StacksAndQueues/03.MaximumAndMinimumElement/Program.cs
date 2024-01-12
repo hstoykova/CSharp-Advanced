8804b11b670cce31595bb125e03073e62d61d5bb
hstoykova <hristina.pencheva@gmail.com>
2024-01-12 13:44:59 +0200

    "04. Fast Food" solved

--- Advanced/A-Exercises-StacksAndQueues/03.MaximumAndMinimumElement/Program.cs
+++ Advanced/A-Exercises-StacksAndQueues/03.MaximumAndMinimumElement/Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
+            // Read n numbers from the console
             int n = int.Parse(Console.ReadLine());
 
+            // Create a Stack
             Stack<int> manipulatedStack = new Stack<int>();
 
+            // Use for to check all n numbers
             for (int i = 0; i < n; i++)
             {
                 int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
@@ -35,6 +38,7 @@
                 }
             }
 
+            // Output:
             Console.WriteLine(string.Join(", ", manipulatedStack));
         }
     }
---
+++ Advanced/A-Exercises-StacksAndQueues/04.FastFood/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+348
+20 54 30 16 7 9
+
+499
+57 45 62 70 33 90 88 76
+ */
+
+namespace _04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int availableFood = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> ordersForTheDay = new Queue<int>(orders);
+
+            Console.WriteLine(ordersForTheDay.Max());
+
+            while (ordersForTheDay.Any())
+            {
+                if (availableFood >= ordersForTheDay.Peek())
+                {
+                    availableFood -= ordersForTheDay.Dequeue();
+                    //Console.WriteLine("Orders complete");
+                }
+                else
+                {
+                    Console.WriteLine("Orders left: " + string.Join(" ", ordersForTheDay));
+                    return;
+                }              
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
