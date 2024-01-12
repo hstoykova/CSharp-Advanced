7a71cee0119b62750ffbb22ab8520bb63df8f349
hstoykova <hristina.pencheva@gmail.com>
2024-01-12 11:06:53 +0200

    "03. Maximum and Minimum Element" solved

---
+++ Advanced/A-Exercises-StacksAndQueues/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03.MaximumAndMinimumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> manipulatedStack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                switch (input[0])
+                {
+                    case 1:
+                        manipulatedStack.Push(input[1]);
+                        break;
+                    case 2:
+                        manipulatedStack.Pop();
+                        break;
+                    case 3:
+                        if (manipulatedStack.Any())
+                        {
+                            Console.WriteLine(manipulatedStack.Max());
+                        }
+                        break;
+                    case 4:
+                        if (manipulatedStack.Any())
+                        {
+                            Console.WriteLine(manipulatedStack.Min());
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", manipulatedStack));
+        }
+    }
+}
