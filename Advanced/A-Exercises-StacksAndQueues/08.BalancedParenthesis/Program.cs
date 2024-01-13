8f3e9c498df6fac6fb2462690c3ff7806fb61cc5
hstoykova <hristina.pencheva@gmail.com>
2024-01-13 17:54:18 +0200

    "08. Balanced Parenthesis" solved

---
+++ Advanced/A-Exercises-StacksAndQueues/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _08.BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> parentheses = new Stack<char>();
+
+            if (input.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ("({[".Contains(input[i]))
+                {
+                    parentheses.Push(input[i]);
+                }
+                else if (input[i] == ')' && parentheses.Peek() == '(')
+                {
+                    parentheses.Pop();
+                }
+                else if (input[i] == ']' && parentheses.Peek() == '[')
+                {
+                    parentheses.Pop();
+                }
+                else if (input[i] == '}' && parentheses.Peek() == '{')
+                {
+                    parentheses.Pop();
+                }
+            }
+
+            if (parentheses.Any())
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+
+            }
+        }
+    }
+}
