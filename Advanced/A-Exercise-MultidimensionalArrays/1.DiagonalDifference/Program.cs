9fa680ad8e361b72da235f28af21f3e7cd2c2e3e
hstoykova <hristina.pencheva@gmail.com>
2024-01-14 11:48:02 +0200

    "1. Diagonal Difference" solved

---
+++ Advanced/A-Exercise-MultidimensionalArrays/1.DiagonalDifference/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+3
+11 2 4
+4 5 6
+10 8 -12
+
+ */
+
+namespace _1.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            // Създаваме матрица
+            int[,] matrix = new int[size,size];
+
+            // Пълним данни в матрицата
+            for (int row = 0; row < size; row++)
+            {
+                int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+
+            int primary = 0;
+            int secondary = 0;
+
+            // Обхождаме матрицата
+
+            for (int row = 0; row < size; row++)
+            {                
+                for (int col = 0; col < size; col++)
+                {
+                    int element = matrix[row, col]; // текущ елемент
+
+                    if (row == col)
+                    {
+                        primary += element;
+                    }
+
+                    if (col == size - 1 - row)
+                    {
+                        secondary += element;
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(primary - secondary));
+        }
+    }
+}
