643a5d51d0b19570c9eb28cd159ad617a74e9b9d
hstoykova <hristina.pencheva@gmail.com>
2024-01-14 20:37:47 +0200

    "2. Squares in Matrix" solved

---
+++ Advanced/A-Exercise-MultidimensionalArrays/2.SquaresInMatrix/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+3 4
+A B B D
+E B B B
+I J B B
+
+2 2
+a b
+c d
+
+ */
+
+using System.Drawing;
+
+namespace _2.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] data = Console.ReadLine().Split(" ");
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+            int count = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col+ 1 ] &&
+                        matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
