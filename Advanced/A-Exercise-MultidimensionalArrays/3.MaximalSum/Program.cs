6a6f7f7a2bfe64dbaa2a6c95847ea7b29dbcaa50
hstoykova <hristina.pencheva@gmail.com>
2024-01-14 21:25:04 +0200

    "3. Maximal Sum" solved

---
+++ Advanced/A-Exercise-MultidimensionalArrays/3.MaximalSum/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+4 5
+1 5 5 2 4
+2 1 4 14 3
+3 7 11 2 8
+4 8 12 16 4
+
+3 2
+1 2
+7 8
+4 9
+
+ */
+
+namespace _3.MaximalSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] data = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+
+            int max = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                        + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (sum > max)
+                    {
+                        max = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine($"Sum = {max}");
+
+            for (int i = maxRow; i < maxRow + 3; i++)
+            {
+                for (int j = maxCol; j < maxCol + 3; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
