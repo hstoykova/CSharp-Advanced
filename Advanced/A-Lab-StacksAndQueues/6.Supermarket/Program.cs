f54bc29a698e4f4a583bd50cbfcd8c55316d7e51
hstoykova <hristina.pencheva@gmail.com>
2024-01-17 20:36:57 +0200

    "6. Supermarket" solved

---
+++ Advanced/A-Lab-StacksAndQueues/6.Supermarket/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Liam
+Noah
+James
+Paid
+Oliver
+Lucas
+Logan
+Tiana
+End
+
+ */
+
+namespace _6.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Queue<string> queue = new Queue<string>();
+
+            while((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    Console.WriteLine(string.Join("\n", queue));
+                    queue.Clear();
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+
+        }
+    }
+}
