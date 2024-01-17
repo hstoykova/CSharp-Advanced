de8286d55e8daa64f6269b1f23dd5182b0691cfe
hstoykova <hristina.pencheva@gmail.com>
2024-01-17 21:22:40 +0200

    "7. Hot Potato" solved

---
+++ Advanced/A-Lab-StacksAndQueues/7.HotPotato/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Alva James William
+2 
+ 
+ */
+
+
+namespace _7.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ");
+            Queue<string> queue = new Queue <string>(names);
+
+            int rounds = int.Parse(Console.ReadLine());
+
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < rounds - 1; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
