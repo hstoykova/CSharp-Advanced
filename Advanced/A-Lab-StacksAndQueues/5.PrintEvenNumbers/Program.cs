885e26a5a1b70b12ee3c5eedfda13cd0e6d6263e
hstoykova <hristina.pencheva@gmail.com>
2024-01-17 20:25:12 +0200

    "5. Print Even Numbers" solved

---
+++ Advanced/A-Lab-StacksAndQueues/5.PrintEvenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+1 2 3 4 5 6
+
+11 13 18 95 2 112 81 46
+
+ */
+namespace _5.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(input);
+            Queue<int> q = new Queue<int>();
+
+
+            while (queue.Count > 0)
+            {
+                int element = queue.Dequeue();
+
+                if (element % 2 == 0)
+                {
+                    q.Enqueue(element);
+                }
+                
+            }
+            
+            Console.WriteLine(string.Join(", ", q));
+        }
+    }
+}
