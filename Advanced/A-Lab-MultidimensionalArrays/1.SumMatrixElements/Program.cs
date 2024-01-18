df370efdce8d88c528d0662c7dd99fd9c5bba0a0
hstoykova <hristina.pencheva@gmail.com>
2024-01-18 08:18:22 +0200

    "1. Sum Matrix Elements" solved

---
+++ Advanced/A-Lab-MultidimensionalArrays/1.SumMatrixElements/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+3, 6
+7, 1, 3, 3, 2, 1
+1, 3, 9, 8, 5, 6
+4, 6, 7, 9, 1, 0 
+ 
+ */
+
+namespace _1.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+
+            //Пълним матрицата с данни от конзолата
+            for (int row = 0; row < rows; row++)
+            {
+                int[] data = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            int sum = 0;
+
+            foreach (var m in matrix)
+            {
+                sum += m;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
