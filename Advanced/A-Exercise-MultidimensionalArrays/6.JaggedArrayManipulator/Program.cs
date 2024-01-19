ffcfe82ae1b72ca625d672736bd9e4482a6b5c0e
hstoykova <hristina.pencheva@gmail.com>
2024-01-19 13:55:13 +0200

    "6. Jagged Array Manipulator" solved

---
+++ Advanced/A-Exercise-MultidimensionalArrays/6.JaggedArrayManipulator/Program.cs
@@ -0,0 +1,105 @@
+﻿/*
+5
+10 20 30
+1 2 3
+2
+2
+10 10
+End
+
+5
+10 20 30
+1 2 3
+2
+2
+10 10
+Add 0 10 10
+Add 0 0 10
+Subtract -3 0 10
+Subtract 3 0 10
+End
+ */
+
+namespace _6.JaggedArrayManipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] cols = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                jaggedArray[row] = cols;
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (jaggedArray[row].Length == jaggedArray[row + 1].Length)
+                {
+                    for (int col = 0; col < jaggedArray[row].Length; col++)
+                    {
+                        jaggedArray[row][col] *= 2;
+                        jaggedArray[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jaggedArray[row].Length; col++)
+                    {
+                        jaggedArray[row][col] /= 2;
+                    }
+                    for (int col = 0; col < jaggedArray[row + 1].Length; col++)
+                    {
+                        jaggedArray[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = tokens[0];
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+                int value = int.Parse(tokens[3]);
+
+                if (ValidCell(row, col, jaggedArray))
+                {
+                    if (action == "Add")
+                    {
+                        jaggedArray[row][col] += value;
+                    }
+                    else
+                    {
+                        jaggedArray[row][col] -= value;
+                    }
+                }
+            }
+
+            // Print matrix
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    Console.Write($"{jaggedArray[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool ValidCell(int row, int col, int[][] jaggedArray)
+        {
+            return
+                row >= 0
+                && row < jaggedArray.Length
+                && col >= 0
+                && col < jaggedArray[row].Length;
+        }
+    }
+}
