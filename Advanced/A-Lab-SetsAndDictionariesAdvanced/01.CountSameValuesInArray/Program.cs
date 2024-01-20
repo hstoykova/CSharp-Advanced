c7bea75d8e40e1b553697ccd387e5955fd0a6485
hstoykova <hristina.pencheva@gmail.com>
2024-01-20 12:46:38 +0200

    "01. Count Same Values in Array" solved

---
+++ Advanced/A-Lab-SetsAndDictionariesAdvanced/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+-2.5 4 3 -2.5 -5.5 4 3 3 -2.5 3
+2 4 4 5 5 2 3 3 4 4 3 3 4 3 5 3 2 5 4 3
+ */
+
+namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
+
+            foreach(var i in input)
+            {
+                if (occurrences.ContainsKey(i))
+                {
+                    occurrences[i]++;
+                }
+                else
+                {
+                    occurrences.Add(i, 1);
+                }
+            }
+
+            foreach (var o in occurrences)
+            {
+                Console.WriteLine($"{o.Key} - {o.Value} times");
+            }
+        }
+    }
+}
