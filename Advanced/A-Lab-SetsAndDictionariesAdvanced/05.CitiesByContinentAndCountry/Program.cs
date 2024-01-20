43db341cf4b0b72c4943551bc59c9fe39ace30f2
hstoykova <hristina.pencheva@gmail.com>
2024-01-20 16:00:20 +0200

    "05. Cities by Continent and Country" solved

---
+++ Advanced/A-Lab-SetsAndDictionariesAdvanced/05.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _05.CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, List<string>>> continents = new();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (continents.ContainsKey(continent))
+                {
+                    if (continents[continent].ContainsKey(country))
+                    {
+                        continents[continent][country].Add(city);
+                    }
+                    else
+                    {
+                        continents[continent].Add(country, new List<string> { city });
+                    }
+                }
+                else
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>> { {country, new List<string> {city} } });
+                }
+            }
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
