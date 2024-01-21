860a5cab8020a12b1e17606b3f3d16227d2de269
hstoykova <hristina.pencheva@gmail.com>
2024-01-21 14:29:20 +0200

    "01. Unique Usernames" solved

---
+++ Advanced/A-Exercise-SetsAndDictionariesAdvanced/01.UniqueUsernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+            }
+
+            foreach (string name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
