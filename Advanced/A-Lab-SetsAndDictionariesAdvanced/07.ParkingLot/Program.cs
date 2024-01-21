52cf3021c37063a03a917b367f4e935bce2b7f3e
hstoykova <hristina.pencheva@gmail.com>
2024-01-21 14:08:16 +0200

    "07. Parking Lot" solved

---
+++ Advanced/A-Lab-SetsAndDictionariesAdvanced/07.ParkingLot/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> cars = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] splitted = input.Split(", ",StringSplitOptions.RemoveEmptyEntries);
+                string command = splitted[0];
+                string carNumber = splitted[1];
+                if (command == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else
+                {
+                    cars.Remove(carNumber);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var number in cars)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
