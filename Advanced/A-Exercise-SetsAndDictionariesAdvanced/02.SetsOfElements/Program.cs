256b541d80ecaeb802f2231ffeb9cb6cfd6597a8
hstoykova <hristina.pencheva@gmail.com>
2024-01-21 14:39:13 +0200

    "02. Sets of Elements" solved

---
+++ Advanced/A-Exercise-SetsAndDictionariesAdvanced/02.SetsOfElements/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+
+            string input = Console.ReadLine();
+            string[] splitted = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int n1 = int.Parse(splitted[0]);
+            int n2 = int.Parse(splitted[1]);
+
+            for (int i = 0; i < n1; i++)
+            {
+                int elements1 = int.Parse(Console.ReadLine());
+                first.Add(elements1);
+            }
+
+            for (int i = 0; i < n2; i++)
+            {
+                int elements2 = int.Parse(Console.ReadLine());
+                second.Add(elements2);
+            }
+
+            Console.WriteLine(string.Join(" ", first.Intersect(second)));
+        }
+    }
+}
