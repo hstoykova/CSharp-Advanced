ec7c41e9338083374650ba0df9a71bb0bf19b697
hstoykova <hristina.pencheva@gmail.com>
2024-01-21 14:23:10 +0200

    "08. SoftUni Party" solved

---
+++ Advanced/A-Lab-SetsAndDictionariesAdvanced/08.SoftUniParty/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> reservations = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                reservations.Add(input);
+            }
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                reservations.Remove(input);
+            }
+
+            Console.WriteLine(reservations.Count);
+
+            foreach (var vip in reservations)
+            {
+                if (vip[0] >= 48 && vip[0] <= 57)
+                {
+                    Console.WriteLine(vip);
+                }
+            }
+
+            foreach (var notvip in reservations)
+            {
+                if (!(notvip[0] >= 48 && notvip[0] <= 57))
+                {
+                    Console.WriteLine(notvip);
+                }                            
+            }
+        }
+    }
+}
