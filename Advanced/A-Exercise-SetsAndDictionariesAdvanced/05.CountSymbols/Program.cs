c77e50099c6592df24b5e25bc03db10aa2cc3db6
hstoykova <hristina.pencheva@gmail.com>
2024-01-21 15:17:42 +0200

    "05. Count Symbols" solved

---
+++ Advanced/A-Exercise-SetsAndDictionariesAdvanced/05.CountSymbols/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> symbols = new Dictionary<char, int>();
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (symbols.ContainsKey(input[i]))
+                {
+                    symbols[input[i]]++;
+                }
+                else
+                {
+                    symbols.Add(input[i], 1);
+                }
+            }
+
+            foreach (var symbol in symbols.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
