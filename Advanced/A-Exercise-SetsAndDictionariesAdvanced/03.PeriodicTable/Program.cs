1ac1ed5aa1671e66ba3efbb3168319edc068daa5
hstoykova <hristina.pencheva@gmail.com>
2024-01-21 14:45:28 +0200

    "03. Periodic Table" solved

---
+++ Advanced/A-Exercise-SetsAndDictionariesAdvanced/03.PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var element in elements)
+                {
+                    set.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", set.OrderBy(x=>x)));
+        }
+    }
+}
