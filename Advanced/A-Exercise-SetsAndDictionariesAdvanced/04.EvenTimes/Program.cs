a8f3ff095f59457017c79c11aaa8af46e7019e82
hstoykova <hristina.pencheva@gmail.com>
2024-01-21 15:02:04 +0200

    "04. Even Times" solved

---
+++ Advanced/A-Exercise-SetsAndDictionariesAdvanced/04.EvenTimes/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (numbers.ContainsKey(number))
+                {
+                    numbers[number]++;
+                }
+                else
+                {
+                    numbers.Add(number, 1);
+                }
+            }
+
+            int output = numbers.Where(x=>x.Value % 2 == 0).First().Key;
+            Console.WriteLine(output);
+        }
+    }
+}
