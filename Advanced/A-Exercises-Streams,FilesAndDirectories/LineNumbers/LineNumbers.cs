940a9b7b54d9a2d2fafcadd83ace44b2bb58f21d
hstoykova <hristina.pencheva@gmail.com>
2024-01-22 10:29:23 +0200

    "01. Even Lines" solved

---
+++ Advanced/A-Exercises-Streams,FilesAndDirectories/LineNumbers/LineNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LineNumbers
+{
+    
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
