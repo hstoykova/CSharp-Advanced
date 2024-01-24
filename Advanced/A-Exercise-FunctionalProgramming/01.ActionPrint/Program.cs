52c2ddcf0b707074105694592327f9e158d573fc
hstoykova <hristina.pencheva@gmail.com>
2024-01-24 15:29:28 +0200

    "01. Action Print" solved

---
+++ Advanced/A-Exercise-FunctionalProgramming/01.ActionPrint/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> names = new List<string>();
+            foreach (string name in input)
+            {
+                names.Add(name);
+            }
+
+            names.ForEach(delegate (string name)
+            {
+                Console.WriteLine(name);
+            });
+        }
+    }
+}
