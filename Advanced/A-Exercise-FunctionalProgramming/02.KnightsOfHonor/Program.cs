469b783eba513a857a5d4ef5cc7a10d3a21f0f85
hstoykova <hristina.pencheva@gmail.com>
2024-01-24 15:34:11 +0200

    "02. Knights of Honor" solved

---
+++ Advanced/A-Exercise-FunctionalProgramming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> names = new List<string>();
+            foreach (string name in input)
+            {
+                names.Add(name);
+            }
+
+            names.ForEach(delegate (string name)
+            {
+                Console.WriteLine($"Sir {name}");
+            });
+        }
+    }
+}
