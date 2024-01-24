b7b56288c3fa03c69498df83fb3ab936c154b617
hstoykova <hristina.pencheva@gmail.com>
2024-01-24 14:40:31 +0200

    "05. Copy Directory"

--- Advanced/A-Exercises-Streams,FilesAndDirectories/CopyDirectory/CopyDirectory.cs
+++ Advanced/A-Exercises-Streams,FilesAndDirectories/CopyDirectory/CopyDirectory.cs
@@ -1,19 +1,35 @@
 ﻿namespace CopyDirectory
 {
     using System;
+    using System.IO;
+
     public class CopyDirectory
     {
         static void Main()
         {
             string inputPath =  @$"{Console.ReadLine()}";
             string outputPath = @$"{Console.ReadLine()}";
-
+            
             CopyAllFiles(inputPath, outputPath);
         }
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
-            throw new NotImplementedException();
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, recursive: true);
+            }
+            
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (string file in files)
+            {
+                string filrName = Path.GetFileName(file);
+                string destination = Path.Combine(outputPath, filrName);
+                File.Copy(file, destination);
+            }
         }
     }
 }
