782ceb64f0bf8de110624e3bfbc08d7ed42e8fbc
hstoykova <hristina.pencheva@gmail.com>
2024-01-25 14:18:49 +0200

    "04. Find Evens or Odds"

---
+++ Advanced/A-Exercise-FunctionalProgramming/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _04.FindEvensOrOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, List<int>> generatedRange = (start, end) =>
+            {
+                List<int> range = new();
+
+                for (int i = start; i <= end; i++)
+                {
+                    range.Add(i);
+                }
+
+                return range;
+            };
+
+            Func<string, int, bool> evenOrOddMatch = (condition, number) =>
+            {
+                if (condition == "even")
+                {
+                    return number % 2 == 0;
+                }
+                else
+                {
+                    return number % 2 != 0;
+                }
+            };
+
+            int[] ranges = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            List<int> numbers = generatedRange(ranges[0], ranges[1]); 
+
+            foreach (int number in numbers)
+            {
+                if (evenOrOddMatch (command, number))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
