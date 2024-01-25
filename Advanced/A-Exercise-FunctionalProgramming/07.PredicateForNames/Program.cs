0d22ffae910264a5f0146f55add71cb53786b713
hstoykova <hristina.pencheva@gmail.com>
2024-01-25 20:35:16 +0200

    "07. Predicate For Names"

---
+++ Advanced/A-Exercise-FunctionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[], Predicate<string>> print = (names, match) =>
+            {
+                foreach (var name in names)
+                {
+                    if (match(name))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            };
+
+            int len = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            print(names, n => n.Length <= len);
+        }
+    }
+}
