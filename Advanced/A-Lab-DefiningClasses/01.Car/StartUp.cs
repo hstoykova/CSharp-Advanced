fffdeb84d0ef41b8bdcbf844aeb246eea29656f3
hstoykova <hristina.pencheva@gmail.com>
2024-01-27 11:28:52 +0200

    "02. Car Extension"

---
+++ Advanced/A-Lab-DefiningClasses/01.Car/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make} {Environment.NewLine}Model: {car.Model} {Environment.NewLine}Year: {car.Year}");
+        }
+    }
+}
---
+++ Advanced/A-Lab-DefiningClasses/02.CarExtension/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer;
+public class Car
+{
+    private string make;
+    private string model;
+    private int year;
+    private double fuelQuantity;
+    private double fuelConsumption;
+
+    public string Make
+    {
+        get { return make; }
+        set { make = value; }
+    }
+
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
+
+    public double FuelQuantity
+    {
+        get { return fuelQuantity; }
+        set { fuelQuantity = value; }
+    }
+
+    public double FuelConsumption
+    {
+        get { return fuelConsumption; }
+        set { fuelConsumption = value; }
+    }
+
+    public void Drive(double distance)
+    {
+        if (FuelQuantity - distance * FuelConsumption >= 0)
+        {
+            FuelQuantity -= distance * FuelConsumption;
+        }
+        else
+        {
+            Console.WriteLine("Not enough fuel to perform this trip!");
+        }
+    }
+
+    public string WhoAmI()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Make: {Make}");
+        sb.AppendLine($"Model: {Model}");
+        sb.AppendLine($"Year: {Year}");
+        sb.AppendLine($"Fuel: {FuelQuantity:F2}");
+
+        return sb.ToString();
+    }
+}
