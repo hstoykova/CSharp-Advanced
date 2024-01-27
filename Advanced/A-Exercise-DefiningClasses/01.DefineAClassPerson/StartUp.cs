eee44e7145dd29d61df73e6e33ef382090904375
hstoykova <hristina.pencheva@gmail.com>
2024-01-27 22:23:33 +0200

    "01. Define a Class Person"

---
+++ Advanced/A-Exercise-DefiningClasses/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+
+        person.Name = "Hristina";
+        person.Age = 31;
+
+        Person hrisi = new Person
+        {
+            Name = "Hristina",
+            Age = 31
+        };
+
+        Console.WriteLine(person.Name);
+    }
+}
