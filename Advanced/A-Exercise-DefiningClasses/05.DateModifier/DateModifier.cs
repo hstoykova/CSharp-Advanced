8a33819f09a24f46542142f6cd6030daf00e656c
hstoykova <hristina.pencheva@gmail.com>
2024-01-29 10:26:15 +0200

    "05. Date Modifier"

---
+++ Advanced/A-Exercise-DefiningClasses/05.DateModifier/DateModifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace DateModifier;
+
+public static class DateModifier
+{
+    public static int CalculateDifference(string start, string end)
+    {
+        DateTime startDate = DateTime.Parse(start);
+        DateTime endDate = DateTime.Parse(end);
+
+        TimeSpan difference = endDate - startDate;
+
+        return Math.Abs(difference.Days);
+    }
+}
+
---
+++ Advanced/A-Exercise-DefiningClasses/05.DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DateModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string start = Console.ReadLine();
+            string end = Console.ReadLine();
+
+            int diff = DateModifier.CalculateDifference(start, end);
+
+            Console.WriteLine(diff);
+        }
+    }
+}
