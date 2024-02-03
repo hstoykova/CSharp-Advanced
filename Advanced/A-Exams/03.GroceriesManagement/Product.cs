6e03082749a00dc85de611d6b88ab52d0147f5b2
hstoykova <hristina.pencheva@gmail.com>
2024-02-03 20:54:09 +0200

    03. Groceries Management

---
+++ Advanced/A-Exams/03.GroceriesManagement/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace GroceriesManagement;
+
+public class Product
+{
+	private string name;
+    private double price;
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
+
+	public double Price
+	{
+		get { return price; }
+		set { price = value; }
+	}
+
+    public override string ToString()
+    {
+        return $"{Name}: {Price:F2}$";
+    }
+}
