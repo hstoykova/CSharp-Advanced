5aae93a82872d1f6f2c7afbbd8c8c3be910a7aef
hstoykova <hristina.pencheva@gmail.com>
2024-02-04 17:41:53 +0200

    "03. Mail Client" solved

---
+++ Advanced/A-Exams/03.MailClient/Mail.cs
@@ -0,0 +1,38 @@
+﻿namespace MailClient;
+
+public class Mail
+{
+	private string sender;
+    private string receiver;
+    private string body;
+
+    public Mail(string sender, string receiver, string body)
+    {
+        Sender = sender;
+        Receiver = receiver;
+        Body = body;
+    }
+
+    public string Sender
+    {
+		get { return sender; }
+		set { sender = value; }
+	}
+
+    public string Receiver 
+    {
+        get { return receiver; }
+        set { receiver = value; }
+    }
+
+	public string Body
+	{
+		get { return body; }
+		set { body = value; }
+	}
+
+    public override string ToString()
+    {
+        return $"From: {Sender} / To: {Receiver}{Environment.NewLine}Message: {Body}";
+    }
+}
---
+++ Advanced/A-Exams/03.MailClient/MailBox.cs
@@ -0,0 +1,95 @@
+﻿using System.Reflection.Metadata;
+
+namespace MailClient;
+
+public class MailBox
+{
+	private int capacity;
+    private List<Mail> inbox;
+    private List<Mail> archive;
+
+    public MailBox(int capacity)
+    {
+        Capacity = capacity;
+        Inbox = new List<Mail>();
+		Archive = new List<Mail>();
+    }
+
+    public int Capacity 
+	{
+		get { return capacity; }
+		set { capacity = value; }
+	}
+
+	public List<Mail> Inbox
+	{
+		get { return inbox; }
+		set { inbox = value; }
+	}
+
+    public List<Mail> Archive
+    {
+		get { return archive ; }
+		set { archive  = value; }
+	}
+
+	public void IncomingMail(Mail mail)
+	{
+		if (Capacity > Inbox.Count)
+		{
+            Inbox.Add(mail);
+
+        }
+	}
+
+	public bool DeleteMail(string sender)
+	{
+		int senderIndex = Inbox.FindIndex(s => s.Sender == sender);
+
+		if (senderIndex < 0)
+		{
+			return false;
+		}
+		else
+		{
+			Inbox.RemoveAt(senderIndex);
+			return true;
+		}
+	}
+
+	public int ArchiveInboxMessages()
+	{
+		int count = 0;
+		while (Inbox.Any())
+		{
+			Mail mail = Inbox.ElementAt(0);
+			Archive.Add(mail);
+			Inbox.RemoveAt(0);
+			count++;
+        }
+		return count;
+    }
+
+	public string GetLongestMessage()
+	{
+		Mail mailIn = Inbox.OrderByDescending(b => b.Body.Length).First();
+		//Mail mailAr = Archive.OrderByDescending(b => b.Body.Length).First();
+
+		return mailIn.ToString();
+
+		//if (mailIn.Body.Length > mailAr.Body.Length)
+		//{
+		//	return mailIn.ToString();
+		//}
+		//else
+		//{
+		//	return mailAr.ToString();
+  //      }
+    }
+	public string InboxView()
+	{
+		return $"Inbox:{Environment.NewLine}{string.Join(Environment.NewLine,Inbox)}";
+		
+    }
+
+}
