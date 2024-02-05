4647937f453298e297db46cb81bb76206a262471
hstoykova <hristina.pencheva@gmail.com>
2024-02-05 09:55:45 +0200

    "03. Generic Swap Method String"

--- Advanced/A-Exercise-Generics/03.GenericSwapMethodString/Program.cs
+++ Advanced/A-Exercise-Generics/03.GenericSwapMethodString/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> items = new();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string item = Console.ReadLine();
+                items.Add(item);
+            }
+
+            int[] indices = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Swap(indices[0], indices[1], items);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+
+            static void Swap<T>(int firstIndex, int secondIndex, List<T> items)
+            {
+                T temp = items[firstIndex];
+                items[firstIndex] = items[secondIndex];
+                items[secondIndex] = temp;
+            }
+
         }
     }
 }
