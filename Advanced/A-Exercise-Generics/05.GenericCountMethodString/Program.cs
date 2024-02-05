0a4e0d7998fcc0cc91f62da066a97f04e23ab84e
hstoykova <hristina.pencheva@gmail.com>
2024-02-05 10:48:38 +0200

    "05. Generic Count Method String"

--- Advanced/A-Exercise-Generics/05.GenericCountMethodString/Program.cs
+++ Advanced/A-Exercise-Generics/05.GenericCountMethodString/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Box<string> box = new ();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string item = Console.ReadLine();
+                box.Add(item);
+            }
+
+            string itemToCompare = Console.ReadLine();
+            Console.WriteLine(box.Count(itemToCompare));
         }
     }
 }
