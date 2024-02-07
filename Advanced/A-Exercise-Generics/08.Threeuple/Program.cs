2f297fb3efc26dda892680a18fb685ff355c0d07
hstoykova <hristina.pencheva@gmail.com>
2024-02-07 16:08:24 +0200

    "08. Threeuple" changed

--- Advanced/A-Exercise-Generics/08.Threeuple/Program.cs
+++ Advanced/A-Exercise-Generics/08.Threeuple/Program.cs
@@ -8,7 +8,7 @@
             string[] drinkTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
             string[] bankTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-            Threeuple<string, string, string> person = new($"{personTokens[0]} {personTokens[1]}", personTokens[2], personTokens[3]);
+            Threeuple<string, string, string> person = new($"{personTokens[0]} {personTokens[1]}", personTokens[2], string.Join(" ", personTokens[3..]));
             Threeuple<string, int, bool> drink = new(drinkTokens[0], int.Parse(drinkTokens[1]), drinkTokens[2] == "drunk");
             Threeuple<string, double, string> bank = new((bankTokens[0]), double.Parse(bankTokens[1]), bankTokens[2]);
 
