a45c4ac8662bfd72d7b179dced986d8c69f46f50
hstoykova <hristina.pencheva@gmail.com>
2024-02-09 21:13:49 +0200

    "03. Stack"

---
+++ Advanced/A-Exercise-IteratorsAndComparators/03.Stack/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _03.Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+            string input;
+
+            string[] delim = { ", ", " " };
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] command = input.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Push")
+                {
+                    for (int i = 1; i < command.Length; i++)
+                    {
+                        stack.Push(command[i]);
+                    }
+                }
+                else if (command[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
