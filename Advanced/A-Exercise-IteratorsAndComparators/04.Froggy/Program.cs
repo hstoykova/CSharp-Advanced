ca080237029fdde09b93a4b8457109cbaeb23a56
hstoykova <hristina.pencheva@gmail.com>
2024-02-09 21:29:09 +0200

    "04. Froggy"

---
+++ Advanced/A-Exercise-IteratorsAndComparators/04.Froggy/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04.Froggy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Lake lake = new Lake(list);
+
+            string result = string.Join(", ", lake);
+            Console.WriteLine(result);
+        }
+    }
+}
