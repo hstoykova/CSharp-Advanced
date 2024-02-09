af453a2e6826f4f66aaa1657aaeecb759ae8d421
hstoykova <hristina.pencheva@gmail.com>
2024-02-09 11:02:48 +0200

    "02. Collection"

---
+++ Advanced/A-Exercise-IteratorsAndComparators/02.Collection/ListyIterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Collection;
+
+public class ListyIterator<T> : IEnumerable<T>
+{
+    private List<T> list;
+    private int index = 0;
+
+    public ListyIterator(List<T> list)
+    {
+        this.list = list;
+    }
+
+    public bool HasNext()
+    {
+        return index < list.Count - 1;
+    }
+
+    public bool Move()
+    {
+        if (HasNext())
+        {
+            index++;
+            return true;
+        }
+        return false;
+    }
+
+    public void Print()
+    {
+        if (list.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        Console.WriteLine(list[index]);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            yield return list[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- Advanced/A-Exercise-IteratorsAndComparators/02.Collection/Program.cs
+++ Advanced/A-Exercise-IteratorsAndComparators/02.Collection/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace _02.Collection
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            ListyIterator<string> listy = new ListyIterator<string>(input.Skip(1).ToList());
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                switch (command)
+                {
+                    case "Print":
+                        try
+                        {
+                            listy.Print();
+                        }
+                        catch (InvalidOperationException) //ioe
+                        {
+                            Console.WriteLine("Invalid Operation!");
+                        }
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+
+                    case "PrintAll":
+                        foreach (var element in listy)
+                        {
+                            Console.Write($"{element} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                }
+            }
         }
     }
 }
