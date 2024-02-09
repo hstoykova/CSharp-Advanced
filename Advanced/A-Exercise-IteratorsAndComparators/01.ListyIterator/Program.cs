d83396be7c3191f8d60684dcedd3c0019d83de23
hstoykova <hristina.pencheva@gmail.com>
2024-02-09 10:25:44 +0200

    "01. ListyIterator"

---
+++ Advanced/A-Exercise-IteratorsAndComparators/01.ListyIterator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            ListyIterator<string> listy = new ListyIterator<string>(input.Skip(1).ToList());
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                switch (command)
+                {
+                    case "Print":
+                        try
+                        {
+                            listy.Print();
+                        }
+                        catch (InvalidOperationException) //ioe
+                        {
+                            Console.WriteLine("Invalid Operation!");
+                        }
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+                }
+            }
+        }
+    }
+}
