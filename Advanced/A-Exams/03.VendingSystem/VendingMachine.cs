f0c1c2044228ae58f456ecfdeee677d83a91e21c
hstoykova <hristina.pencheva@gmail.com>
2024-02-10 18:38:09 +0200

    "03. Vending System"

---
+++ Advanced/A-Exams/03.VendingSystem/VendingMachine.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingSystem;
+
+public class VendingMachine
+{
+    public VendingMachine(int buttonCapacity)
+    {
+        ButtonCapacity = buttonCapacity;
+        Drinks = new List<Drink>();
+    }
+
+    public int ButtonCapacity { get; set; }
+
+    public List<Drink> Drinks { get; set; }
+
+    public int GetCount
+    {
+        get
+        {
+            return Drinks.Count;
+        }
+    }
+
+    public void AddDrink(Drink drink)
+    {
+        if (ButtonCapacity > GetCount)
+        {
+            Drinks.Add(drink);
+        }
+    }
+
+    public bool RemoveDrink(string name)
+    {
+        int index = Drinks.FindIndex(x => x.Name == name);
+
+        if (index == -1)
+        {
+            return false;
+        }
+        else
+        {
+            Drinks.RemoveAt(index);
+            return true;
+        }
+    }
+
+    public Drink GetLongest()
+    {
+        return Drinks.OrderByDescending(d => d.Volume).First();
+    }
+
+    public Drink GetCheapest()
+    {
+        return Drinks.OrderBy(d => d.Price).First();
+    }
+
+    public string BuyDrink(string name)
+    {
+        return Drinks.Find(x => x.Name == name).ToString();
+    }
+
+    public string Report()
+    {
+        StringBuilder sb = new();
+        sb.AppendLine("Drinks available:");
+        Drinks.ForEach(d => sb.AppendLine(d.ToString()));
+        return sb.ToString().Trim();
+    }
+}
