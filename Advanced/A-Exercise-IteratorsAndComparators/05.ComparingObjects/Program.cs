0542e94316164636fdb7077b3ce6766ec1fe812a
hstoykova <hristina.pencheva@gmail.com>
2024-02-12 10:03:23 +0200

    "05. Comparing Objects"

---
+++ Advanced/A-Exercise-IteratorsAndComparators/05.ComparingObjects/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _05.ComparingObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] personProp = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new();
+                {
+                    person.Name = personProp[0];
+                    person.Age = int.Parse(personProp[1]);
+                    person.Town = personProp[2];
+                };
+
+                people.Add(person);
+            }
+
+            int index = int.Parse(Console.ReadLine()) - 1;
+            Person indexPerson = people[index];
+
+            int equalCount = 0;
+            int diffCount = 0;
+
+            foreach (Person person in people)
+            {
+                if (person.CompareTo(indexPerson) == 0)
+                {
+                    equalCount++;
+                }
+                else
+                {
+                    diffCount++;
+                }
+            }
+
+            if (equalCount == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalCount} {diffCount} {people.Count}");
+            }
+        }
+    }
+}
