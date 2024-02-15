b76bf85f1f47fad1af3c4f1e91b7b7fde0bdad5f
hstoykova <hristina.pencheva@gmail.com>
2024-02-15 20:31:58 +0200

    "02. The Squirrel"

---
+++ Advanced/A-Exams/01.RubberDuckDebugers/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _01.RubberDuckDebugers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> programmerTime = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> numberOfTasks = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int dartDCount = 0;
+            int thorDCount = 0;
+            int bbRubberDCount = 0;
+            int smallYRDCount = 0;
+
+            while (programmerTime.Any() && numberOfTasks.Any())
+            {
+                int currentTime = programmerTime.Dequeue();
+                int currentTask = numberOfTasks.Pop();
+                int result = currentTime * currentTask;
+
+                if (result >= 0 && result <= 60)
+                {
+                    dartDCount++;
+                }
+                else if (result >= 61 && result <= 120)
+                {
+                    thorDCount++;
+                }
+                else if (result >= 121 && result <= 180)
+                {
+                    bbRubberDCount++;
+                }
+                else if (result >= 181 && result <= 240)
+                {
+                    smallYRDCount++;
+                }
+                else
+                {
+                    currentTask -= 2;
+                    numberOfTasks.Push(currentTask);
+                    programmerTime.Enqueue(currentTime);
+                }
+            }
+
+            Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+            Console.WriteLine($"Darth Vader Ducky: {dartDCount}");
+            Console.WriteLine($"Thor Ducky: {thorDCount}");
+            Console.WriteLine($"Big Blue Rubber Ducky: {bbRubberDCount}");
+            Console.WriteLine($"Small Yellow Rubber Ducky: {smallYRDCount}");
+        }
+    }
+}
---
+++ Advanced/A-Exams/02.TheSquirrel/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace _02.TheSquirrel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] commands = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = n;
+            int cols = n;
+
+            int currentPositionRow = -1;
+            int currentPositionCol = -1;
+
+            int hazelnutsCount = 0;
+
+            bool isTrapped = false;
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] data = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = data[col];
+
+                    if (data[col] == 's')
+                    {
+                        currentPositionRow = row;
+                        currentPositionCol = col;
+                    }
+                }
+            }
+
+            foreach (var direction in commands)
+            {
+                int nextPositionRow = currentPositionRow;
+                int nextPositionCol = currentPositionCol;
+
+                if (direction == "up")
+                {
+                    nextPositionRow -= 1;
+                }
+                if (direction == "down")
+                {
+                    nextPositionRow += 1;
+                }
+                if (direction == "left")
+                {
+                    nextPositionCol -= 1;
+                }
+                if (direction == "right")
+                {
+                    nextPositionCol += 1;
+                }
+
+                if (nextPositionRow >= rows || nextPositionCol >= cols || nextPositionRow < 0 || nextPositionCol < 0)
+                {
+                    isTrapped = true;
+                    Console.WriteLine("The squirrel is out of the field.");
+                    break;
+                }
+
+                currentPositionRow = nextPositionRow;
+                currentPositionCol = nextPositionCol;
+
+                char element = matrix[currentPositionRow, currentPositionCol];
+
+                if (element == 't')
+                {
+                    isTrapped = true;
+                    Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+                    break;
+                }
+
+                if (element == 'h')
+                {
+                    matrix[currentPositionRow, currentPositionCol] = '*';
+                    hazelnutsCount++;
+
+                    if (hazelnutsCount == 3)
+                    {
+                        Console.WriteLine("Good job! You have collected all hazelnuts!");
+                        break;
+                    }
+                }
+            }
+
+            if (!isTrapped && hazelnutsCount < 3)
+            {
+                Console.WriteLine("There are more hazelnuts to collect.");
+            }
+
+            Console.WriteLine($"Hazelnuts collected: {hazelnutsCount}");
+        }
+    }
+}
