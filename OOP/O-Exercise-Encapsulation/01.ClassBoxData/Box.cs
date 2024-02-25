7daf1add3c28903771bb09720aa06e3f93773260
hstoykova <hristina.pencheva@gmail.com>
2024-02-25 12:11:43 +0200

    "01. Class Box Data" solved

---
+++ OOP/O-Exercise-Encapsulation/01.ClassBoxData/Box.cs
@@ -0,0 +1,74 @@
+﻿
+
+namespace ClassBoxData;
+
+public class Box
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height)
+    {
+        Length = length;
+        Width = width;
+        Height = height;
+    }
+
+    public double Length
+    {
+        get { return length; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Length cannot be zero or negative.");
+            }
+            length = value;
+        }
+    }
+
+    public double Width
+    {
+        get { return width; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width cannot be zero or negative.");
+            }
+        }
+    }
+
+    public double Height
+    {
+        get { return height; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height cannot be zero or negative.");
+            }
+        }
+    }
+
+    public double SurfaceArea(double length, double width, double heihht)
+    {
+        return 2 * length * width + 2 * length * heihht + 2 * width * heihht;
+        // 2lw + 2lh + 2wh
+    }
+
+    public double LateralSurfaceArea(double length, double width, double heihht)
+    {
+        return 2 * length * heihht + 2 * width * heihht;
+        //2lh + 2wh
+    }
+
+    public double Volume(double length, double width, double heihht)
+    {
+        return length * width * heihht;
+        //lwh
+    }
+}
+
+
