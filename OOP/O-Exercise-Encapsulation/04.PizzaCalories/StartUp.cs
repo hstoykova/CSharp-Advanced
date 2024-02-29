9fa9f4c253cfc403ca7fd5c15fae671e749c9ad0
hstoykova <hristina.pencheva@gmail.com>
2024-02-29 15:10:31 +0200

    "04. Pizza Calories"

---
+++ OOP/O-Exercise-Encapsulation/04.PizzaCalories/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using PizzaCalories.Models;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] pizzaTokens = Console.ReadLine().Split();
+                string[] doughTokens = Console.ReadLine().Split();
+
+                string pizzaName = pizzaTokens[1];
+
+                Dough dough = new(doughTokens[1], doughTokens[2], double.Parse(doughTokens[3]));
+
+                Pizza pizza = new(pizzaName, dough);
+
+                while (true)
+                {
+                    string toppingsInput = Console.ReadLine();
+
+                    if (toppingsInput == "END")
+                    {
+                        break;
+                    }
+
+                    string[] toppingsTokens = toppingsInput.Split();
+
+                    Topping topping = new(toppingsTokens[1], double.Parse(toppingsTokens[2]));
+
+                    pizza.AddToping(topping);
+
+                }
+
+                Console.WriteLine(pizza);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
