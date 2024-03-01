0be04b859a295bfcc94e448895d57ed43a2f999f
hstoykova <hristina.pencheva@gmail.com>
2024-03-01 11:20:33 +0200

    "02. Cars" solved

---
+++ OOP/O-Lab-InterfacesAndAbstraction/02.Cars/ICar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cars;
+
+public interface ICar
+{
+    public string Model { get; set; }
+    public string Color { get; set; }
+
+    string Start();
+    string Stop();
+}
