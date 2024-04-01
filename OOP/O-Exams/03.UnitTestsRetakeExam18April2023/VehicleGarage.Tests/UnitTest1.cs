9bc0fe9b30e95fac1d17619c3a86fe6eb4720169
hstoykova <hristina.pencheva@gmail.com>
2024-04-01 22:03:25 +0300

    SoftUni exam prep

--- OOP/O-Exams/03.UnitTestsRetakeExam18April2023/VehicleGarage.Tests/UnitTest1.cs
+++ OOP/O-Exams/03.UnitTestsRetakeExam18April2023/VehicleGarage.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace VehicleGarage.Tests
 {
@@ -19,7 +21,7 @@ namespace VehicleGarage.Tests
             Assert.AreEqual(0, garage.Vehicles.Count);
         }
         [Test]
-        public void AddVehicleReturnsFalseWhenCapacityIsReached() 
+        public void AddVehicleReturnsFalseWhenCapacityIsReached()
         {
             Garage garage = new(0);
             Vehicle vehicle = new("VW", "Polo", "SFG123");
@@ -52,7 +54,112 @@ namespace VehicleGarage.Tests
             vehicle.BatteryLevel = 10;
             garage.AddVehicle(vehicle);
             Assert.AreEqual(1, garage.ChargeVehicles(20));
+            Assert.IsTrue(garage.Vehicles.All(v => v.BatteryLevel == 100));
+        }
+
+        [Test]
+        public void ChargeVehiclesReturnZero()
+        {
+            Garage garage = new(5);
+            Vehicle vehicle = new("VW", "Polo", "SFG123");
+            vehicle.BatteryLevel = 50;
+            garage.AddVehicle(vehicle);
+            Assert.AreEqual(0, garage.ChargeVehicles(20));
+            //Assert.IsTrue(garage.Vehicles.All(v => v.BatteryLevel == 100));
+        }
+
+        [Test]
+        public void DriveVehicleCheckBatteryLevelChange()
+        {
+            Garage garage = new(5);
+            Vehicle vehicle = new("VW", "Polo", "SFG123");
+            vehicle.BatteryLevel = 100;
+            garage.AddVehicle(vehicle);
+            garage.DriveVehicle("SFG123", 20, false);
+
+            Assert.AreEqual(80, vehicle.BatteryLevel);
+            Assert.IsFalse(vehicle.IsDamaged);
+        }
+
+        [Test]
+        public void DriveVehicleCheckIsDamagedChange()
+        {
+            Garage garage = new(5);
+            Vehicle vehicle = new("VW", "Polo", "SFG123");
+            vehicle.BatteryLevel = 100;
+            garage.AddVehicle(vehicle);
+            garage.DriveVehicle("SFG123", 20, true);
+
+            Assert.AreEqual(80, vehicle.BatteryLevel);
+            Assert.IsTrue(vehicle.IsDamaged);
+        }
+
+        [Test]
+        public void DriveVehicleCheck1()
+        {
+            Garage garage = new(5);
+            Vehicle vehicle = new("VW", "Polo", "SFG123");
+            vehicle.BatteryLevel = 100;
+            vehicle.IsDamaged = true;
+            garage.AddVehicle(vehicle);
+            garage.DriveVehicle("SFG123", 20, false);
+
             Assert.AreEqual(100, vehicle.BatteryLevel);
+            Assert.IsTrue(vehicle.IsDamaged);
+        }
+
+        [Test]
+        public void DriveVehicleCheck2()
+        {
+            Garage garage = new(5);
+            Vehicle vehicle = new("VW", "Polo", "SFG123");
+            vehicle.BatteryLevel = 100;
+            garage.AddVehicle(vehicle);
+            garage.DriveVehicle("SFG123", 120, false);
+
+            Assert.AreEqual(100, vehicle.BatteryLevel);
+            Assert.IsFalse(vehicle.IsDamaged);
+        }
+
+        [Test]
+        public void DriveVehicleCheck3()
+        {
+            Garage garage = new(5);
+            Vehicle vehicle = new("VW", "Polo", "SFG123");
+            vehicle.BatteryLevel = 10;
+            garage.AddVehicle(vehicle);
+            garage.DriveVehicle("SFG123", 20, false);
+
+            Assert.AreEqual(10, vehicle.BatteryLevel);
+            Assert.IsFalse(vehicle.IsDamaged);
+        }
+
+        [Test]
+        public void DriveVehicleCheck4()
+        {
+            Garage garage = new(5);
+            Assert.Throws<NullReferenceException>(() => garage.DriveVehicle("SFG123", 20, false));
+        }
+
+        [Test]
+        public void RepairVehiclesCheck()
+        {
+            Garage garage = new(5);
+            Vehicle vehicle = new("VW", "Polo", "SFG123");
+            vehicle.IsDamaged = true;
+            garage.AddVehicle(vehicle);
+
+            Vehicle vehicle1 = new("VW", "Polo", "kkk123");
+            vehicle1.IsDamaged = true;
+            garage.AddVehicle(vehicle1);
+
+            Vehicle vehicle2 = new("VW", "Polo", "888888");
+            garage.AddVehicle(vehicle2);
+
+            string result = garage.RepairVehicles();
+
+            Assert.AreEqual("Vehicles repaired: 2", result);
+            Assert.IsTrue(garage.Vehicles.All(v => !v.IsDamaged));
         }
     }
 }
